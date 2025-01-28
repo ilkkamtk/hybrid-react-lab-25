b12472d0ca867791b12c9f0197f658b942f422d2
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-01-28 11:12:18 +0200

    add example view and fetch media data from JSON; update routing and create fetch utility

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Profile from './views/Profile';
 import Upload from './views/Upload';
 import Layout from './components/Layout';
 import Single from './views/Single';
+import Example from './views/Example';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
             <Route path="/profile" element={<Profile />} />
             <Route path="/upload" element={<Upload />} />
             <Route path="/single" element={<Single />} />
+            <Route path="/example" element={<Example />} />
           </Route>
         </Routes>
       </BrowserRouter>
