6afc4c6683132ac87d2a4e938cfe677044ddd749
mattpe <mattpe@metropolia.fi>
2025-02-12 10:19:26 +0200

    add reducer function

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,6 +3,7 @@ import MediaRow from '../components/MediaRow';
 import {useState} from 'react';
 import SingleView from '../components/SingleView';
 import {useMedia} from '../hooks/apiHooks';
+import TryReducerCounter from '../components/TryReducerCounter';
 
 const Home = () => {
   const [selectedItem, setSelectedItem] = useState<
